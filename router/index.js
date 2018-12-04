4e987aca8b3524ee6fa917becc05d87207c9f606
xiongjian <webjsforyou@gmail.com>
2018-12-04 15:34:20 +0800

    登录路由

--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const md5 = require('blueimp-md5');
 const Users = require('../model/users');
 
 const router = new express.Router();
@@ -28,7 +29,7 @@ router.post('/register', async (req, res) => {
     } else {
       //用户可以注册
       //保存在数据库中
-      const user = await Users.create({username, password, type});
+      const user = await Users.create({username, password: md5(password), type});
       //返回成功的响应
       res.json({
         code: 0,
@@ -37,6 +38,41 @@ router.post('/register', async (req, res) => {
           _id: user.id,
           type: user.type
         }
+      })
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({
+      code: 2,
+      msg: '网络不稳定，请刷新试试~'
+    })
+  }
+})
+//登录
+router.post('/login', async (req, res) => {
+  //获取用户提交请求参数信息
+  const {username, password} = req.body;
+  console.log(username, password);
+  
+  try {
+    //去数据库查找当前用户是否存在
+    const user = await Users.findOne({username, password: md5(password)});
+    if (user) {
+      //用户可登录，登录成功
+      res.json({
+        code: 0,
+        data: {
+          _id: user.id,
+          type: user.type,
+          username: user.username
+        }
+      })
+    } else {
+      //用户名或密码错误
+      //返回失败的响应
+      res.json({
+        code: 1,
+        msg: '用户名或密码错误'
       })
     }
   } catch (e) {
