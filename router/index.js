b82c58da219d2b1f632710866f2325bdcc61bdb1
xiongjian <webjsforyou@gmail.com>
2018-12-06 11:52:04 +0800

    获取用户信息路由

--- router/index.js
+++ router/index.js
@@ -129,4 +129,29 @@ router.post('/update', (req, res) => {
     })
 })
 
+// 获取用户信息的路由(根据cookie中的userid)
+router.get('/user', (req, res) => {
+  // 从请求的cookie得到userid
+  const userid = req.cookies.userid
+  // 如果不存在, 直接返回一个提示信息
+  if (!userid) {
+    return res.send({code: 1, msg: '请先登陆'})
+  }
+  // 根据userid查询对应的user
+  Users.findOne({_id: userid}, {__v: 0, password: 0})
+    .then(user => {
+      if (user) {
+        res.send({code: 0, data: user})
+      } else {
+        // 通知浏览器删除userid cookie
+        res.clearCookie('userid')
+        res.send({code: 1, msg: '请先登陆'})
+      }
+    })
+    .catch(error => {
+      console.error('获取用户异常', error)
+      res.send({code: 1, msg: '获取用户异常, 请重新尝试'})
+    })
+})
+
 module.exports = router;
